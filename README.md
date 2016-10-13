0d3d34d48e2a16d918e64dc16945abe5df7b8ac7
dianyingx007 <dianyingx007@163.com>
2016-10-13 16:58:51 +0800

    url to object URL参数转对象

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#js中的一些实现
+js中的一些实现
 
 ##deepClone.js 深度克隆对象
 
@@ -7,3 +7,8 @@
 * function的exception
 * 对象的深度克隆中Array类型的判断
 * 克隆函数的递归调用
+
+##url2obj.js url参数转对象
+方法：
+1. 正则匹配
+2. 识别字符'?'和'&'
---
+++ url2obj.js
@@ -0,0 +1,29 @@
+//url地址转对象
+var url='http://www.baidu.com/index.html?id=dianying&key0=gg';
+
+function url2obj(url){
+    var reg=/(\w+)=(\w+)/g;
+    var obj={};
+    var arr;
+    while((arr=reg.exec(url))!==null){
+        obj[arr[1]]=arr[2];
+    }
+    return obj;
+}
+console.log('方法一：');
+console.log(url2obj(url));
+
+function url2obj2(url){
+    var index=url.indexOf('?');
+    var obj={};
+    if(index!==-1){
+        var arr=url.slice(index+1).split('&');
+        for(var i=0;i<arr.length;i++){
+            var url_kv=arr[i].split('=');
+            obj[url_kv[0]]=url_kv[1];
+        }
+    }
+    return obj;
+}
+console.log('方法二：');
+console.log(url2obj2(url));
